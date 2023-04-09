1fff6bddab721931d4d7cd2059f97df1c8938db4
Chris Davidson <chrisdavidson@ChristophersAir.lan>
2023-04-09 15:50:00 -0400

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+    def hello
+        render html: "Hello, Chris! Nice work making your first Ruby on Rails app! GET THAT MONEY"
+    end 
+
+    def goodbye
+        render html: "You got this. You can do this. This is the way."
+    end 
+
+end
